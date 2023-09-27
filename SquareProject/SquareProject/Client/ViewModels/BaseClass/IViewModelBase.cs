50678d1a8eed7ba8b60c9a2df57cf98eb39c6981
szt2kehsza <115459926+szt2kehsza@users.noreply.github.com>
2023-09-27 18:58:24 +0200

    ViewModels kész

---
+++ SquareProject/SquareProject/Client/ViewModels/BaseClass/IViewModelBase.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace SquareProject.Client.ViewModels.BaseClass
+{
+    public interface IViewModelBase : INotifyPropertyChanged
+    {
+        public bool IsBusy { get; set; }
+    }
+}
